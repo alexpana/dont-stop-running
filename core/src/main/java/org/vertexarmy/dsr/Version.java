b7e2c9eb2dcee5e2ead08cbd298709715c48e494
Alexandru Pana <astronothing@gmail.com>
2015-03-27 00:28:13 +0200

    Level Editor: Added initial versioning

---
+++ core/src/main/java/org/vertexarmy/dsr/Version.java
@@ -0,0 +1,16 @@
+package org.vertexarmy.dsr;
+
+/**
+ * created by Alex
+ * on 3/27/2015.
+ */
+public class Version {
+    public static final int MAJOR = 1;
+    public static final int MINOR = 0;
+
+    public static final String releaseName = "Hatchling";
+
+    public static String value() {
+        return MAJOR + "." + MINOR + " \"" + releaseName + "\"";
+    }
+}
