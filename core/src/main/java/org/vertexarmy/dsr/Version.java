c7cd93a857c473b1f8ce9c48c6d45f83b33ba210
Alexandru Pana <astronothing@gmail.com>
2015-03-29 10:14:16 +0300

    Updated version

--- core/src/main/java/org/vertexarmy/dsr/Version.java
+++ core/src/main/java/org/vertexarmy/dsr/Version.java
@@ -6,7 +6,7 @@ package org.vertexarmy.dsr;
  */
 public class Version {
     public static final int MAJOR = 1;
-    public static final int MINOR = 0;
+    public static final int MINOR = 1;
 
     public static final String releaseName = "Hatchling";
 
